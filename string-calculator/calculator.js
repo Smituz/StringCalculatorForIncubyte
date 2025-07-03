96010af286560638ecf819733054cf486c878ab8
Smituz <smitbhansali0308@gmail.com>
2025-07-04 02:30:04 +0530

    Make test pass for strings having \n as delimiter

--- string-calculator/calculator.js
+++ string-calculator/calculator.js
@@ -6,10 +6,11 @@ function add(numbers){
 
     //implentation logic for input string having single number or two numbers comma seperated as input 
 
-    //splitting and parsing the array to get input number/s as int
-    var numArray = numbers.split(',');
+    //splitting and parsing the array to get input number/s as int [Delimiters allowed currently are "\n" & ","]
+    var numArray = numbers.replace(/\n/g, ',').split(',');
     numArray = numArray.map(n => parseInt(n,10));
 
     return numArray.reduce((sum, val) => sum + val, 0);
 }
+
 module.exports={ add }; 
