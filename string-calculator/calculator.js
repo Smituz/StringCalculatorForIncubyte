5d7245b6e042bc80f2dc8f28c19e2040ee243840
Smituz <smitbhansali0308@gmail.com>
2025-07-03 23:33:45 +0530

    Make test pass for input string with single number in it

--- string-calculator/calculator.js
+++ string-calculator/calculator.js
@@ -3,7 +3,10 @@ function add(numbers){
     //condition check for empty string
     if(numbers === "")
         return 0;
-    
+    //implementation logic for string including a single decimal number
+    console.log(typeof(numbers))
+    if(!numbers.includes(','))
+        return parseInt(numbers, 10);
 }
 
 module.exports={ add }; 
