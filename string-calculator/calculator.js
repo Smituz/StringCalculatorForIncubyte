874f574f78ecf5064f291a9a24c18d8f8a14f30a
Smituz <smitbhansali0308@gmail.com>
2025-07-04 03:01:54 +0530

    Make test pass for strings having custom delimeter

--- string-calculator/calculator.js
+++ string-calculator/calculator.js
@@ -7,6 +7,14 @@ function add(numbers){
     //implentation logic for input string having single number or two numbers comma seperated as input 
 
     //splitting and parsing the array to get input number/s as int [Delimiters allowed currently are "\n" & ","]
+    if(numbers.startsWith("//")){
+        var delimiter = numbers[2];
+        numbers = numbers.split("//" + delimiter + "\n").join("");
+        var numArray = numbers.split(delimiter);
+        numArray = numArray.map(n => parseInt(n,10));
+
+        return numArray.reduce((sum, val) => sum + val, 0);
+    }
     var numArray = numbers.replace(/\n/g, ',').split(',');
     numArray = numArray.map(n => parseInt(n,10));
 
