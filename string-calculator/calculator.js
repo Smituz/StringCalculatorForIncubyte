79fce613dee7b9fcb7cb7fdec78a6f6e9398f157
Smituz <smitbhansali0308@gmail.com>
2025-07-03 23:50:22 +0530

    Make test pass for strings with two single numbers, comma seperated

--- string-calculator/calculator.js
+++ string-calculator/calculator.js
@@ -3,9 +3,15 @@ function add(numbers){
     //condition check for empty string
     if(numbers === "")
         return 0;
+
     //implementation logic for string including a single decimal number
     if(!numbers.includes(','))
         return parseInt(numbers, 10);
+
+    //implementation for strings with two single numbers which are comma seperated
+    if(numbers.includes(',')){
+        return (parseInt(numbers[0],10) + parseInt(numbers[2],10));
+    }
 }
 
 module.exports={ add }; 
