2c9abbd5b34b7aa7afe80d29f6a5189e7ce22426
Smituz <smitbhansali0308@gmail.com>
2025-07-03 22:53:52 +0530

    Make test pass for empty string input

---
+++ string-calculator/calculator.js
@@ -0,0 +1,7 @@
+//function to add numbers give in string
+function add(numbers){
+    if(numbers === "")
+        return 0;
+}
+
+module.exports={ add }; 
--- string-calculator/calculator.test.js
+++ string-calculator/calculator.test.js
@@ -1,5 +1,7 @@
+//Getting the add function from ./calculator.js
+const { add } = require('./calculator');
 
-//Empty string Test
+//Empty string Test 
 test('returns 0 for an empty string', ()=>{
     expect(add("")).toBe(0);
 });
