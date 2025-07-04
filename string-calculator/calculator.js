c2c0f4af45716e0b346208d412639d315c855262
Smituz <smitbhansali0308@gmail.com>
2025-07-04 20:53:52 +0530

    Make test pass by adding 'checkNegatives' Helper function

--- string-calculator/calculator.js
+++ string-calculator/calculator.js
@@ -3,11 +3,13 @@ function add(numbers){
     //Return 0 for an empty field
     if(numbers === "")
         return 0;
-    //Check for string having a single negative number
-    if(numbers.includes("-")){
-            let negativeNumber = -(parseInt(numbers[numbers.indexOf('-',0) + 1 ]));
-            throw new Error("negatives not allowed: "+ negativeNumber + ".");    
-    }
+    // Check for string having a single negative number
+    // if(numbers.includes("-")){
+    //         let negativeNumber = -(parseInt(numbers[numbers.indexOf('-',0) + 1 ]));
+    //         throw new Error("negatives not allowed: "+ negativeNumber + ".");    
+    // }
+
+    //checkNegatives Helper function now handles the above case
 
     //If input starts with custom delimiter, extract it and split accordingly.
     if(numbers.startsWith("//")){
@@ -26,7 +28,19 @@ function add(numbers){
 function sumParsednumbers(numArray){
     var numArray = numArray.map(n => parseInt(n,10));
 
+    //Checking presence of negative numbers
+    checkNegatives(numArray);
+
     return numArray.reduce((sum, val) => sum + val, 0);
 }
 
+//Helper function to throw exception for negatives
+function checkNegatives(numArray){
+    const negatives = numArray.filter(n => n<0);
+
+    if(negatives.length > 0){
+        throw new Error("negatives not allowed: "+ negatives + ".");   
+    }
+    return;
+}
 module.exports={ add }; 
