e062449571d3e191d308a4a8de2e8e6e1b6d7384
Smituz <smitbhansali0308@gmail.com>
2025-07-05 01:49:48 +0530

    Refactoring : Introduce StringCalculator class and update tests to use class-based approach

--- string-calculator/calculator.js
+++ string-calculator/calculator.js
@@ -1,64 +1,81 @@
-//function to add numbers given in a string
-function add(numbers){
-    //Return 0 for an empty field
-    if(numbers === "")
-        return 0;
+//Object-Oriented programming
+class StringCalculator {
 
-    //Dealing with custom delimiters in different formats 
-    if (numbers.startsWith("//")) {
-    let delimiters = [];
+    constructor() {
+        this.callCount = 0;
+    }
 
-    if (numbers.startsWith("//[")) {
-        // Match all custom delimiters between square brackets
-        const delimiterMatches = numbers.match(/\/\/(\[.*?\])+\n/);
-        if (delimiterMatches) {
-            // Extract all delimiters
-            const delimiterPattern = /\[(.*?)\]/g;
-            let match;
-            while ((match = delimiterPattern.exec(delimiterMatches[0])) !== null) {
-                delimiters.push(match[1]);
+    //function to add numbers given in a string
+    add(numbers) {
+        //Increment callCount to keep track of invocations
+        this.callCount++;
+
+        //Return 0 for an empty field
+        if (numbers === "")
+            return 0;
+
+        //Dealing with custom delimiters in different formats 
+        if (numbers.startsWith("//")) {
+            let delimiters = [];
+
+            if (numbers.startsWith("//[")) {
+                // Match all custom delimiters between square brackets
+                const delimiterMatches = numbers.match(/\/\/(\[.*?\])+\n/);
+                if (delimiterMatches) {
+                    // Extract all delimiters
+                    const delimiterPattern = /\[(.*?)\]/g;
+                    let match;
+                    while ((match = delimiterPattern.exec(delimiterMatches[0])) !== null) {
+                        delimiters.push(match[1]);
+                    }
+                }
+                numbers = numbers.split('\n').slice(1).join('\n'); // Remove delimiter declaration line
+            } else {
+                // Single-character delimiter (no brackets)
+                delimiters.push(numbers[2]);
+                numbers = numbers.slice(4); // Remove //x\n
             }
+
+            // Build a regex to split using all delimiters
+            const splitRegex = new RegExp(delimiters.map(d => this.#escapeRegex(d)).join('|'), 'g');
+            const numArray = numbers.split(splitRegex);
+            return this.#sumParsednumbers(numArray);
         }
-        numbers = numbers.split('\n').slice(1).join('\n'); // Remove delimiter declaration line
-    } else {
-        // Single-character delimiter (no brackets)
-        delimiters.push(numbers[2]);
-        numbers = numbers.slice(4); // Remove //x\n
+        // Split and parse the input into integers using the allowed delimiters: "," and "\n".
+        var numArray = numbers.replace(/\n/g, ',').split(',');
+        return this.#sumParsednumbers(numArray);
     }
 
-    // Build a regex to split using all delimiters
-    const splitRegex = new RegExp(delimiters.map(d => escapeRegex(d)).join('|'), 'g');
-    const numArray = numbers.split(splitRegex);
-    return sumParsednumbers(numArray);
-}
-    // Split and parse the input into integers using the allowed delimiters: "," and "\n".
-    var numArray = numbers.replace(/\n/g, ',').split(',');
-    return sumParsednumbers(numArray);
-}
-
-//Helper function which parses and sums numbers from the string while filtering >1000 and checking for negatives.
-function sumParsednumbers(numArray){
-    var numArray = numArray.map(n => parseInt(n,10)).filter(n => n<=1000);
-
-    //Checking presence of negative numbers
-    checkNegatives(numArray);
-
-    return numArray.reduce((sum, val) => sum + val, 0);
-}
-
-//Helper function to throw exception for negatives.
-function checkNegatives(numArray){
-    const negatives = numArray.filter(n => n<0);
-
-    if(negatives.length > 0){
-        throw new Error("negatives not allowed: "+ negatives + ".");   
+    //returns number of add() invocations
+    getCalledCount() {
+        return this.callCount;
     }
-    return;
-}
 
-//Helper function to escape special regex characters in delimiters
-function escapeRegex(s) {
-    return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
-}
+    //Private(#) Helper functions to help some main functions of the class
 
-module.exports={ add }; 
+    //Helper function which parses and sums numbers from the string while filtering >1000 and checking for negatives.
+    #sumParsednumbers(numArray) {
+        var numArray = numArray.map(n => parseInt(n, 10)).filter(n => n <= 1000);
+
+        //Checking presence of negative numbers
+        this.#checkNegatives(numArray);
+
+        return numArray.reduce((sum, val) => sum + val, 0);
+    }
+
+    //Helper function to throw exception for negatives.
+    #checkNegatives(numArray) {
+        const negatives = numArray.filter(n => n < 0);
+
+        if (negatives.length > 0) {
+            throw new Error("negatives not allowed: " + negatives + ".");
+        }
+        return;
+    }
+
+    //Helper function to escape special regex characters in delimiters
+    #escapeRegex(s) {
+        return s.replace(/[-/\\^$*+?.()|[\]{}]/g, '\\$&');
+    }
+}
+module.exports = { StringCalculator }; 
--- string-calculator/calculator.test.js
+++ string-calculator/calculator.test.js
@@ -1,47 +1,53 @@
-//Getting the add function from ./calculator.js
-const { add } = require('./calculator');
+//This was before introduction of StringCalculator Class
+/*Getting the .add function from ./calculator.js
+    const { add } = require('./calculator');*/
+
+//Getting the StringCalculator class from ./calculator.js
+const { StringCalculator } = require("./calculator");
+
+const calculator = new StringCalculator();
 
 //Test for Empty Input String  
 test('returns 0 for an empty string', ()=>{
-    expect(add("")).toBe(0);
+    expect(calculator.add("")).toBe(0);
 });
 
 //Test for Single Digit Input String
 test('returns number itself when only one number provided in the string', ()=>{
-    expect(add("1")).toBe(1);
+    expect(calculator.add("1")).toBe(1);
 });
 
 //Test for strings having two numbers which are comma seperated
-test('returns addition of two numbers in the string which are comma seperated',()=>{
-    expect(add("3,2")).toBe(5);
+test('returns calculator.addition of two numbers in the string which are comma seperated',()=>{
+    expect(calculator.add("3,2")).toBe(5);
 }); 
 
 //Test for strings having unknown amount of numbers which are comma seperated
 test('returns summation of all the list of comma-seperated numbers in the given string', ()=>{
-    expect(add("2,35,56,1")).toBe(94);
-    expect(add("111,22,44")).toBe(177);
+    expect(calculator.add("2,35,56,1")).toBe(94);
+    expect(calculator.add("111,22,44")).toBe(177);
 });
 
 //Test for strings having "," as well as  "\n" delimiters
 test('returns summation of all the numbers in the string having "," as well as "\n" as delimiters', ()=>{
-    expect(add("1,2\n3,4\n15")).toBe(25);
+    expect(calculator.add("1,2\n3,4\n15")).toBe(25);
 });
 
 //Test for strings having custom delimiter
 test('returns summation of numbers having a custom delimiter',()=>{
-    expect(add("//&\n1&2&97")).toBe(100);
+    expect(calculator.add("//&\n1&2&97")).toBe(100);
 });
 
 //Test for strings having a single negative interger
 test('throws exception for a single negative number', ()=>{
-    expect(() => add("5,3,2,-3,8")).toThrow("negatives not allowed: -3.");
-    expect(() => add("//;\n1;-3;4")).toThrow("negatives not allowed: -3.");
+    expect(() => calculator.add("5,3,2,-3,8")).toThrow("negatives not allowed: -3.");
+    expect(() => calculator.add("//;\n1;-3;4")).toThrow("negatives not allowed: -3.");
 });
 
 //Test for strings having multiple negative intergers
 test('throws exception for strings having multiple negative numbers', ()=>{
-    expect(() => add("5,3,-2,-3,8")).toThrow("negatives not allowed: -2,-3.");
-    expect(() => add("//;\n-1;-3;4")).toThrow("negatives not allowed: -1,-3.");
+    expect(() => calculator.add("5,3,-2,-3,8")).toThrow("negatives not allowed: -2,-3.");
+    expect(() => calculator.add("//;\n-1;-3;4")).toThrow("negatives not allowed: -1,-3.");
 });
 
 
@@ -50,20 +56,20 @@ test('throws exception for strings having multiple negative numbers', ()=>{
 
 //Test for string containing numbers greater than 1000. The numbers should be ignored in calculation
 test('all numbers in the input string greater than 1000 are ignored', ()=>{
-    expect(add("1,10000,1000,2,4,1")).toBe(1008);
+    expect(calculator.add("1,10000,1000,2,4,1")).toBe(1008);
 });
 
 //Test for custom delimiter of any length in the input string
 test('accepts custom delimiter of any length in the input string', ()=>{
-     expect(add("//[&&]\n1&&2&&97")).toBe(100);
-     expect(add("//[&&&]\n1&&&2&&&97")).toBe(100);
+     expect(calculator.add("//[&&]\n1&&2&&97")).toBe(100);
+     expect(calculator.add("//[&&&]\n1&&&2&&&97")).toBe(100);
 
 });
 
 //Test for multiple custom delimiters of any length
 test('accepts multiple custom delimiters of any length in the input string',()=>{
-    expect(add("//[&&][;]\n1;2&&97")).toBe(100);
-    expect(add("//[&&][###]\n1&&2###97")).toBe(100);
+    expect(calculator.add("//[&&][;]\n1;2&&97")).toBe(100);
+    expect(calculator.add("//[&&][###]\n1&&2###97")).toBe(100);
 
 });
 
@@ -73,4 +79,5 @@ test('returns count on how many times add() was invoked using getCalledCount()',
     calc.add("1,2\n,3");
     calc.add("//[&&]\n1&&2&&97");
     calc.add("");
+    expect(calc.getCalledCount()).toBe(3);
 });
